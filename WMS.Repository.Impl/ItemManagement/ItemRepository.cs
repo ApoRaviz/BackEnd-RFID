fc75998be89bfe716c916496b3a29b09a6cdad82
job <kanawut.kp@gmail.com>
2017-10-16 08:56:04 +0700

    add repo

--- WMS.Repository.Impl/ItemManagement/ItemRepository.cs
+++ WMS.Repository.Impl/ItemManagement/ItemRepository.cs
@@ -5,26 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using WIM.Core.Repository;
 using WIM.Core.Repository.Impl;
+using WMS.Common;
 using WMS.Context;
 using WMS.Entity.ItemManagement;
+using WMS.Repository.ItemManagement;
 
 namespace WMS.Repository.Impl.ItemManagement
 {
-    public class ItemRepository
+    public class ItemRepository : IItemRepository
     {
         private WMSDbContext Db { get; set; }
-        private IGenericRepository<Item_MT> Repo { get; set; }
 
         public ItemRepository()
         {
-            Repo = new GenericRepository<Item_MT>(Db);
+            Db = new WMSDbContext();
         }
 
         public IEnumerable<ItemDto> GetItems()
         {
-            IEnumerable<Item_MT> items = (from i in db.Item_MT
+            IEnumerable<ItemDto> items = (from i in Db.Item_MT
                                           where i.Active == 1
-                                          select i).ToList();            
+                                          select new ItemDto
+                                          {
+                                               ItemName = i.ItemName
+                                          }).ToList();
+            return items;
         }
     }
 }
