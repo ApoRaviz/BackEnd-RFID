ff3a0d43bd17350e0bd96ba3e273e92c36fb12b4
Job <kanawutp@gmail.com>
2017-11-06 11:43:42 +0700

    add the master project

---
+++ Master.Common.ValueObject/UnitDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Master.Master;
+
+namespace Master.Common.ValueObject
+{
+    public class UnitDto 
+    {
+        public UnitDto()
+        {
+            this.Item_Unit = new HashSet<ItemUnitDto>();
+        }
+
+        public int UnitIDSys { get; set; }
+        public int ProjectIDSys { get; set; }
+        public string UnitID { get; set; }
+        public string UnitName { get; set; }
+        public byte Active { get; set; }
+
+        public virtual ICollection<ItemUnitDto> Item_Unit { get; set; }
+    }
+}
