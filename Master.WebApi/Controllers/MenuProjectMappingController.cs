670371554142c6665fe8fc99bc7a4b07484abaac
Suchat Inyam <pamankug01@gmail.com>
2017-11-27 14:28:08 +0700

    commit change menu

--- Master.WebApi/Controllers/MenuProjectMappingController.cs
+++ Master.WebApi/Controllers/MenuProjectMappingController.cs
@@ -371,7 +371,7 @@ namespace Master.WebApi.Controllers
                 if (User.IsSysAdmin())
                 {
                     MenuPermis = MenuProjectMappingService.GetMenuProjectByID(ProID,Db);
-                    MenuRes = MenuPermis.Where(x => x.MenuIDSysParent != 0 && x.Url != null).GroupBy(x => x.MenuName).Select(grp => grp.First()).ToList();
+                    MenuRes = MenuPermis.Where(x => /*x.MenuIDSysParent != 0 &&*/ x.Url != null).GroupBy(x => x.MenuName).Select(grp => grp.First()).ToList();
                 }
                 else
                 {
