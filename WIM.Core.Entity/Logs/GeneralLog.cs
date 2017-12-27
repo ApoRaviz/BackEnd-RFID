316e4049d000aa6f406e97b3cd7ab024ff3f0b94
danupon <danupont@yamatothai.com>
2017-12-27 10:47:41 +0700

    Commit update code fuji

---
+++ WIM.Core.Entity/Logs/GeneralLog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WIM.Core.Common.Utility.Extentions;
+
+namespace WIM.Core.Entity.Logs
+{
+    [Table("GeneralLogs")]
+    public class GeneralLog : BaseEntity
+    {
+
+        public GeneralLog(string empID)
+        {
+            CreateBy = UpdateBy = empID;
+            CreateAt = UpdateAt = DateTime.Now;
+        }
+        public GeneralLog(string propName, object obj, string createAndUpdateBy, string remark = "")
+        {
+            Type typeObj = obj.GetType();
+
+            List<string> namePropKeys = typeObj.GetPropertiesName<KeyAttribute>(obj);
+            string[] keys = new string[namePropKeys.Count];
+            for (int i = 0; i < namePropKeys.Count; i++)
+            {
+                keys[i] = typeObj.GetProperty(namePropKeys[i]).GetValue(obj, null) + "";
+            }
+
+            TableName = typeObj.GetAttributeValue((TableAttribute dna) => dna.Name);
+            ColumnName = propName;
+            RefID = string.Join(",", keys);
+            Value = typeObj.GetProperty(propName).GetValue(obj, null) + "";
+            Remark = remark;
+            CreateBy = UpdateBy = createAndUpdateBy;
+            CreateAt = UpdateAt = DateTime.Now;
+            IsActive = true;
+        }
+
+        [Key]
+        public int LogIDSys { get; set; }
+        public string TableName { get; set; }
+        public string ColumnName { get; set; }
+        public string RefID { get; set; }
+        public string Value { get; set; }
+        public string Remark { get; set; }
+
+    }
+}
