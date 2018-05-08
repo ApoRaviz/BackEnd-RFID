d62998c204431731d0c0cdce480a08bd5dd469e3
pramchk <pramchk@gmail.com>
2018-05-08 09:05:57 +0700

    8/5/61

---
+++ WIM.Core.Service/TableControl/ITableControlService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WIM.Core.Entity.TableControl;
+
+namespace WIM.Core.Service
+{
+    public interface ITableControlService : IService
+    {
+        IEnumerable<TableControl> GetTableControl();
+    }
+}
