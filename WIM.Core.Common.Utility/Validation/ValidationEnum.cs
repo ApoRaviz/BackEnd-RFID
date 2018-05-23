eb28c612b48a89a36c24b990ec0c5ab4aab51b59
danupon <danupont@yamatothai.com>
2018-05-23 13:40:50 +0700

    Commit edit validation

---
+++ WIM.Core.Common.Utility/Validation/ValidationEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using WIM.Core.Common.Utility.Attributes;
+
+namespace WIM.Core.Common.Utility.Validation
+{
+    public enum ValidationEnum
+    {
+        [ValueEnum("A01")]
+        Required,
+        [ValueEnum("B01")]
+        Email,
+        [ValueEnum("C01")]
+        MaxLength,
+        [ValueEnum("C02")]
+        MinLength,
+    }
+}
