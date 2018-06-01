76dba3e4f435c0029c1d18d30b0963922286ab43
Itthiwat Jinwarachanud <lamsinghanea@hotmail.com>
2018-06-01 08:13:35 +0700

    Update UserLog and LogController

--- WIM.Core.Service/Common/ICommonService.cs
+++ WIM.Core.Service/Common/ICommonService.cs
@@ -4,6 +4,7 @@ using WIM.Core.Common.ValueObject;
 using WIM.Core.Entity.Common;
 using WIM.Core.Entity.SupplierManagement;
 using WIM.Core.Service;
+using WMS.Common.ValueObject;
 
 namespace WIM.Core.Service
 {
@@ -14,7 +15,9 @@ namespace WIM.Core.Service
         bool WriteUserLog(UserLog log);
         IEnumerable<UserLog> UserLogData();
         IEnumerable<UserLog> GetUserLogData(int? logId, DateTime? RequestDateFrom, DateTime? RequestDateTo);
-        IEnumerable<UserLog> GetUserLogData(string RequestMethod, string RequestUrl, string RequestUrlFrontEnd, DateTime? RequestDateFrom, DateTime? RequestDateTo);
+        IEnumerable<UserLog> GetUserLogData(string RequestMethod, string RequestUrl, string RequestUrlFrontEnd, string RequestMenuNameFrontEnd, DateTime? RequestDateFrom, DateTime? RequestDateTo);
+        IEnumerable<UserLog> GetUserLogData2(LogMasterParameters logMasterParameters);
+        IEnumerable<UserLogDto> GetUserLogData3(LogMasterParameters logMasterParameters);
         IEnumerable<TableColumnsDescription> GetTableColumnsDescription(string TableName);
         string GetDataAutoComplete(string columnNames, string tableName, string conditionColumnNames, string keyword);
         IEnumerable<SubModuleDto> SMAutoComplete(string key);
---
+++ WMS.Common.ValueObject/LogMasterPaging.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WIM.Core.Entity.Common;
+
+namespace WMS.Common.ValueObject
+{
+    public class LogMasterPaging
+    {
+        public LogMasterPaging(int totalrow, IEnumerable<UserLogDto> logData)
+        {
+            this.Totalrow = totalrow;
+            this.logData = logData;
+        }
+        public int Totalrow { get; set; }
+        public IEnumerable<UserLogDto> logData { get; set; }
+    }
+}
